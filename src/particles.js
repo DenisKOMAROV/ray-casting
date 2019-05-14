f87bcb2d6a2b834a59571e16ac73532ae4894633
DenisKOMAROV <komarov976@mail.ru>
2019-05-15 01:07:25 +0300

    casual all-in-one commit, working version

---
+++ src/particles.js
@@ -0,0 +1,45 @@
+class Particle {
+	constructor() {
+		this.position = createVector(width/2, height/2);
+		this.rays = [];
+
+		const degree = 0.6; // change rays casting
+		for (let angle = 0; angle < 360; angle += degree) {
+			this.rays.push(new Ray(this.position, radians(angle)));
+		}
+	}
+
+	show() {
+		fill(255);
+		ellipse(this.position.x, this.position.y, 4);
+		for (let ray of this.rays) {
+			ray.show();
+		}
+	}
+
+	look(walls) {
+		for (let ray of this.rays) {
+			let closest = null;
+			let record = Infinity;
+			for (let wall of walls) {
+				const point = ray.cast(wall);
+				if (point) {
+					const distance = p5.Vector.dist(this.position, point);
+					if (distance < record) {
+						record = distance;
+						closest = point;
+					}
+				}
+			}
+			if (closest) {
+				// transparency
+				stroke(255, 100);
+				line(this.position.x, this.position.y, closest.x, closest.y);
+			}
+		}
+	}
+
+	update(x, y) {
+		this.position.set(x, y);
+	}
+}
--- src/sketch.js
+++ src/sketch.js
@@ -1,22 +1,38 @@
-let wall;
+let walls = [];
 let ray;
+let particle;
+let xOffset = 0;
+let yOffset = 10000;
 
 function setup() {
 	createCanvas(400, 400);
-	wall = new Boundary(300, 100 , 300, 300, );
-	ray = new Ray(100, 200);
+
+	for (let i = 0; i < 5; i++) {
+		let x1 = random(width);
+		let y1 = random(height);
+		let x2 = random(width);
+		let y2 = random(height);
+		walls[i] = new Boundary(x1, y1, x2, y2);
+	}
+	// add edge walls
+	walls.push(new Boundary(0, 0, width, 0));
+	walls.push(new Boundary(width, 0, width, height));
+	walls.push(new Boundary(width, height, 0, height));
+	walls.push(new Boundary(0, 0, 0, height));
+
+
+	particle = new Particle();
 }
 
 function draw() {
 	background(0);
-	wall.show();
-	ray.show();
-	ray.lookAt(mouseX, mouseY);
-
-	const point = ray.cast(wall);
-	console.log(point);
-	if (point) {
-		fill(255);
-		ellipse(point.x, point.y, 8, 8);
+	for (let wall of walls) {
+		wall.show();
 	}
+	particle.update(noise(xOffset) * width, noise(yOffset) * height);
+	particle.show();
+	particle.look(walls);
+
+	xOffset += 0.01;
+	yOffset += 0.01;
 }
